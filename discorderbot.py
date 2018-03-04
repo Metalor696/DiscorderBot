5efe3d308676211d49001533de42e6f2d2f3b8ed
Metalor696 <metalor@gmail.com>
2018-03-04 21:12:09 +1300

    Set global variable in on_ready function
    clean query string before sending to chatterbot

--- discorderbot.py
+++ discorderbot.py
@@ -10,8 +10,6 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 DISCORD_BOT_KEY = os.environ.get('DISCORD_BOT_KEY')
 print(DISCORD_BOT_KEY)
 
-# Uncomment the following lines to enable verbose logging
-
 logging.basicConfig(level=logging.INFO)
 
 # Create a new ChatBot instance
@@ -42,27 +40,26 @@ bot.train("chatterbot.corpus.english")
 CONVERSATION_ID = bot.storage.create_conversation()
 
 async def getResponse(content):
-    print('trying to get response from botty')
     return bot.get_response(content)
 
+## need to re-implement this
 async def checkForTriggerMatch(query, triggers):
     for t in triggers:
-        print('the trigger is: ' + t)
         if(query.lower().startswith(t)):
-            print('in the query loop')
             replying = True
             await removeBotReference(query, t)
 
-async def removeBotReference(query, wordList):
-    for word in wordList:    
-        src_str  = re.compile(word, re.IGNORECASE)     
-        query  = src_str.sub('', query)
-    return query
+async def removeBotReference(query, queryToLower, wordList):
+    for word in wordList: 
+        if(queryToLower.startswith(word)):
+            src_str  = re.compile(word, re.IGNORECASE)     
+            query  = src_str.sub('', query)
+            return query
 
 # Create Discord client - This will wrap our Chatbot and read all input but only send a response if the bot is being spoken to
 client = discord.Client()
 
-#set discordBot references to be accessed once ready is fired
+#set global discordBot references to be accessed once ready is fired
 botName = ''
 botNameCleaned = ''
 botId = 0
@@ -77,50 +74,36 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+    global botName
     botName = client.user.name
+    global botNameCleaned
     botNameCleaned = ''.join(e for e in botName if e.isalnum())
+    global botId
     botId = client.user.id
 
-    print('botName is '+ botName)
-    print('botNameCleaned is '+ botNameCleaned)
-    print('botid is ' + str(botId))
-
     #define trigger terms for our bot
-    triggers = [botName, botNameCleaned,'@'+str(botId), 'hey @' +str(botId), 'hey ' +botName, 'hey ' +botNameCleaned, 'hi ' +botName, 'hi ' +botNameCleaned, 'oi ' +botName, 'oi ' +botNameCleaned, '!'+botName, '!'+botNameCleaned]
+    global triggers
+    triggers = [botName, botNameCleaned,'<@'+str(botId)+'>', 'hey <@' +str(botId)+'>', 'hey ' +botName, 'hey ' +botNameCleaned, 'hi ' +botName, 'hi ' +botNameCleaned, 'oi ' +botName, 'oi ' +botNameCleaned, '!'+botName, '!'+botNameCleaned]
+    global triggersLower
     triggersLower = [x.lower() for x in triggers]
-
-    print(triggersLower)
 
 @client.event
 async def on_message(message):
-    #hacky implemenation for now
-    botName = client.user.name
-    botNameCleaned = ''.join(e for e in botName if e.isalnum())
-    botId = client.user.id
-
-    triggers = [botName, botNameCleaned,'@'+str(botId), 'hey @' +str(botId), 'hey ' +botName, 'hey ' +botNameCleaned, 'hi ' +botName, 'hi ' +botNameCleaned, 'oi ' +botName, 'oi ' +botNameCleaned, '!'+botName, '!'+botNameCleaned]
-    triggersLower = [x.lower() for x in triggers]
-
-
     user = message.author if message.author else message.user
-    print('User.name is '+user.name)
-    if(user.name != client.user.name):
+    if(user.name != client.user.name): # Checking that the message is not from our bot - we don't want it replying to itself into infinity!
         replying = False
         queryString = message.content
-        print('the query string is' +queryString)
+        queryStringToLower = queryString.lower()
 
-        print('on message the triggersLower are: ')
-        print(triggersLower)
-
-        if any(map(queryString.lower().startswith, triggersLower)):
+        #check if Bot has been summoned.
+        if any(map(queryStringToLower.startswith, triggersLower)):
             replying = True
-            queryString = await removeBotReference(queryString, triggersLower)
+            queryString = await removeBotReference(queryString, queryStringToLower, triggersLower)
         
+        #clean string before persisting to DB
+        queryString = queryString.lstrip(" ,.?!;][}{%@$^&*")
         response = await getResponse(queryString)
-        print('The response from botty is ')
-        print(response)        
 
-        print('replying is ' +str(replying))
         # Here we only reply if replying is set to true
         if replying:
             await client.send_message(message.channel, response)
